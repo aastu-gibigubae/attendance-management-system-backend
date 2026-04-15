030bf598ee917dd746eff107559149940f5dab00
nathnaeltamirat <nathnaeltamirat3@gmail.com>
2026-04-15 19:46:59 +0300

    remove caching

--- app.js
+++ app.js
@@ -1,38 +1,48 @@
 // app.js  (CommonJS version)
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require("cookie-parser"); 
+const cookieParser = require("cookie-parser");
 
-const authRouter = require("./routes/auth.route"); 
+const authRouter = require("./routes/auth.route");
 const courseRouter = require("./routes/course.route");
 const enrollmentRouter = require("./routes/enrollment.route");
 const attendanceRouter = require("./routes/attendance.route");
 const studentRouter = require("./routes/student.route");
-const analyticsRouter = require("./routes/analytics.route");;
+const analyticsRouter = require("./routes/analytics.route");
 const adminRouter = require("./routes/adminRoutes");
 
 const app = express();
 app.set("trust proxy", 1);
+app.use((req, res, next) => {
+  res.setHeader(
+    "Cache-Control",
+    "no-store, no-cache, must-revalidate, proxy-revalidate",
+  );
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
+  next();
+});
 app.use(
   cors({
-    origin: ["https://attendance.aastugibigubae.com", "https://attendance-managment-system-bice.vercel.app"],
+    origin: [
+      "https://attendance.aastugibigubae.com",
+      "https://attendance-managment-system-bice.vercel.app",
+    ],
     credentials: true,
-  })
+  }),
 );
-
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
-
 app.use("/api/v1", authRouter);
-app.use("/api/v1/course",courseRouter);
-app.use("/api/v1/enrollment",enrollmentRouter);
-app.use("/api/v1/attendance",attendanceRouter);
-app.use("/api/v1/student",studentRouter);
-app.use("/api/v1/admin", adminRouter)
+app.use("/api/v1/course", courseRouter);
+app.use("/api/v1/enrollment", enrollmentRouter);
+app.use("/api/v1/attendance", attendanceRouter);
+app.use("/api/v1/student", studentRouter);
+app.use("/api/v1/admin", adminRouter);
 
-app.use("/api/v1/analytics",analyticsRouter);
+app.use("/api/v1/analytics", analyticsRouter);
 
 module.exports = app;
